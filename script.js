3f02209a0a818ca184c87b03e49ddd78502b01b8
Corina Andariese <candariese@gmail.com>
2018-04-10 23:26:23 -0700

    Completed assignment, all solutions passed tests

--- script.js
+++ script.js
@@ -1,30 +1,83 @@
 module.exports = {
-  difference: function(){
-
+  difference: function(a,b) {
+    return (a - b);
   },
-  isEqual: function(){
 
+  isEqual: function(a,b) {
+    if(a !== b) {
+      return false;
+    }
+    return true;
   },
-  isEven: function(){
 
+  isEven: function(i) {
+    if(i % 2 === 0) {
+      return true;
+    }
+    return false;
   },
-  isDivisible: function(){
 
+  isDivisible: function(a,b) {
+    if(a % b === 0) {
+      return true;
+    }
+    return false;
   },
-  discountPercentage: function(){
 
+  discountPercentage: function(cost,discount) {
+    if(discount < 0 || discount > 100) {
+      return 'please enter a number between 0 and 100';
+    }
+    let result = (discount / 100) * cost;
+      return result;
   },
-  isVowel: function(){
 
+  isVowel: function(letter) {
+    switch(letter) {
+      case 'a' :
+      case 'e' :
+      case 'i' :
+      case 'o' :
+      case 'u' :
+        return true;
+      break;
+      default:
+        return false;
+    }
   },
-  celsiusToFahrenheit: function(){
 
+  celsiusToFahrenheit: function(c) {
+    let f = (c * 1.8) + 32;
+      return f;
   },
-  biggestOfThree: function(){
 
+  biggestOfThree: function(a,b,c) {
+    let max = Math.max(a,b,c);
+      return max;
   },
   // Bonus
-  federalIncomeTaxCalculator: function(){
 
+  federalIncomeTaxCalculator: function(salary,status) {
+    if ((status !== 'single') && (status !== 'joint')) {
+        return 'Better call an accountant';
+    }
+    if (salary > 74900) {
+        return 'Better call an accountant'
+    }
+    if (status == 'joint') {
+        if (salary <= 18650) {
+            return '10%';
+        } else {
+            return '15%';
+        }
+    } else {
+        if (salary < 9325) {
+            return '10%';
+        } else if (salary <= 37950) {
+            return '15%';
+        } else {
+            return '25%';
+        }
+    }
   }
-};
+}
